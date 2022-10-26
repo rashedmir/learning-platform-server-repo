24c99af99daad640cbc31dc66dc676209c99051f
rashedmir <rashedmir89@gmail.com>
2022-10-27 02:34:14 +0600

    Courses added

--- index.js
+++ index.js
@@ -3,9 +3,39 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+
+const categories = require('./data/category.json');
+const courses = require('./data/courses.json');
+
 app.get('/', (req, res) => {
     res.send('Learning API running');
 });
+
+app.get('/course-categories', (req, res) => {
+    res.send(categories)
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '00') {
+        res.send(courses)
+    }
+    else {
+        const course_category = courses.filter(n => n.category_id === id);
+        res.send(course_category);
+    }
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find(c => c._id === id);
+    res.send(selectedCourse);
+})
 
 app.listen(port, () => {
     console.log('Learning server running on port', port);
